062a68e929f820b1a5950e9da0d0eadd1511c574
Santiago Casini <santiago.casini@gmail.com>
2026-02-04 19:05:04 -0300

    Some notes

--- Assets/Scripts/BattleManager.cs
+++ Assets/Scripts/BattleManager.cs
@@ -1,5 +1,11 @@
 using UnityEngine;
 
+// Pone personajes en el campo de batalla, determina según x criterio quién se mueve primero (por ahora, random).
+//- HealthComponent: Determina que un objeto puede ser dañado.
+//- AttackComponent: Determina que un objeto puede atacar a otro.
+//- PlayerController: Mueve un personaje jugable según input.
+//- EnemyController: Elige una acción dependiendo de un behavior tree.
+
 public class BattleManager : MonoBehaviour
 {
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -12,5 +18,15 @@ public class BattleManager : MonoBehaviour
     void Update()
     {
         
+    }
+
+    void SetRandomEncounter()
+    {
+   
+    }
+
+    void SetScriptedEncounter()
+    {
+
     }
 }
