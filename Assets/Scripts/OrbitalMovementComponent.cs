98391d8507e4557d798c5ddb356eb8df214142bd
Santiago Casini <santiago.casini@gmail.com>
2026-02-18 13:33:29 -0300

    Flank sample

--- Assets/Scripts/OrbitalMovementComponent.cs
+++ Assets/Scripts/OrbitalMovementComponent.cs
@@ -9,6 +9,7 @@ public class OrbitalMovementComponent : MonoBehaviour
     public float speed = 1f; // The speed of the orbit
     public float initialAngle = 0f; // The initial angle of the orbit in degrees
     public bool tidalLock = false; // If true, the object will always face the center point
+    public bool autoStart = true; // If true, the orbit will start automatically
 
     void Start()
     {
@@ -16,8 +17,26 @@ public class OrbitalMovementComponent : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (autoStart)
+        {
+            AutoOrbit();
+        }
+
+    }
+
+    void AutoOrbit() {         if (centerPoint == null) return; // Ensure there is a center point to orbit around
+        // Calculate the current angle based on time and speed
+        float angle = initialAngle + Time.time * speed;
+        float radians = angle * Mathf.Deg2Rad; // Convert angle to radians
+        // Calculate the new position in the orbit
+        Vector3 offset = new Vector3(Mathf.Cos(radians), 0, Mathf.Sin(radians)) * radius;
+        transform.position = centerPoint.position + offset;
+        // If tidal lock is enabled, rotate the object to always face the center point
+        if (tidalLock)
+        {
+            transform.LookAt(centerPoint);
+        }
     }
 }
